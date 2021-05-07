0fa9d347f155b17882adfef13f20e797607159a2
OneTwoThree-hub <82404639+OneTwoThree-hub@users.noreply.github.com>
2021-05-07 13:56:17 -0400

    final changes

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    tiles.setTilemap(tilemap`City2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.over(true)
+})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (true) {
-        mySprite.vy += -100
+    if (jumpCount < 1) {
         jumpCount += 1
+        mySprite.vy = -150
     }
+    pause(2000)
+    jumpCount = 0
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    tiles.setTilemap(tilemap`City2`)
+    tiles.setTilemap(tilemap`CityScape2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath5, function (sprite, location) {
     game.over(false)
@@ -15,15 +25,21 @@ let mySprite: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setTilemap(tilemap`CityScape`)
 mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
-controller.moveSprite(mySprite)
+info.setLife(1)
+controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
+mySprite.ay = 200
+forever(function () {
+    if (true) {
+    	
+    }
+})
 forever(function () {
     mySprite.ay = 200
-    jumpCount += 0
 })
 forever(function () {
     if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-    	
+        jumpCount += 0
     }
 })
