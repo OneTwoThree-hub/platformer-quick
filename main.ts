383685ae0574532d81f9df199c23d650383b64ec
OneTwoThree-hub <82404639+OneTwoThree-hub@users.noreply.github.com>
2021-05-13 13:57:48 -0400

    added sprites and code

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
-    tiles.setTilemap(tilemap`City2`)
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.over(true)
-})
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (jumpCount < 1) {
         jumpCount += 1
@@ -13,23 +6,30 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     pause(2000)
     jumpCount = 0
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    tiles.setTilemap(tilemap`CityScape2`)
-    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
+scene.onOverlapTile(SpriteKind.Player, assets.image`paintTwo`, function (sprite, location) {
+    tiles.setTilemap(tilemap`City2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 9))
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.castle.tilePath5, function (sprite, location) {
     color.FadeToWhite.startScreenEffect(1000)
     game.over(false)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.image`paintOne`, function (sprite, location) {
+    tiles.setTilemap(tilemap`CityScape2`)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 9))
+})
+scene.onOverlapTile(SpriteKind.Player, assets.image`paintThree`, function (sprite, location) {
+    game.over(true)
+})
 let jumpCount = 0
 let mySprite: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setTilemap(tilemap`CityScape`)
-mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`myImage1`, SpriteKind.Player)
 info.setLife(1)
 controller.moveSprite(mySprite, 100, 0)
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 10))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(2, 9))
 mySprite.ay = 200
 forever(function () {
     if (true) {
